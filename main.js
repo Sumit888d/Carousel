68e305bfda404e053f9ac4eda3c44370294bbbf4
Sumit888d <77741088+Sumit888d@users.noreply.github.com>
2023-06-21 23:26:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+const slides=document.querySelectorAll(".slide");
+let counter=0;
+
+slides.forEach(
+    (slide,index)=>{
+        slide.style.left=`${index*100}%`;
+    }
+)
+
+const slideImage=()=>{
+    slides.forEach(
+        (slide)=>{
+            slide.style.transform=`translateX(-${counter*100}%)`;
+        }
+    )
+}
+
+const goPrev=()=>{
+    if (counter == 0) {
+        counter = slides.length - 1;
+        slideImage();
+    } else {
+        counter--;
+        slideImage();
+    }
+}
+
+const goNext=()=>{
+    if (counter == slides.length - 1) {
+        counter = 0;
+        slideImage();
+    } else {
+        counter++;
+        slideImage();
+    }
+}
+
+function del(){
+setTimeout(function (){
+   window.location.reload();
+}, 750);
+}
+
+function NewImg(){
+    del();
+}
